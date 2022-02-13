ae94c6e24d38063c514e25d5180dbe043c96a03e
Soliperso <chah762002@yahoo.fr>
2022-02-13 11:26:45 -0800

    Add form validation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,77 @@
 import { useState } from 'react';
 
 const App = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+    newsLetter: true,
+  });
+
+  const handleChange = (e) => {
+    const { name, value, checked, type } = e.target;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: type === 'checkbox' ? checked : value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (formData.password === formData.confirmPassword) {
+      console.log('Passwords match');
+    } else {
+      console.log('Passwords do not match');
+      return;
+    }
+
+    if (formData.newsLetter) {
+      console.log('You will receive our newsletter');
+    }
+  };
+
   return (
     <div className='form-container'>
-      <form className='form'>
-        <input type='text' name='email' placeholder='Email address' className='form__input' />
-        <input type='password' name='password' placeholder='Password' className='form__input' />
+      <form className='form' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='email'
+          placeholder='Email address'
+          className='form__input'
+          name='email'
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          name='password'
+          placeholder='Password'
+          className='form__input'
+          name='password'
+          value={formData.password}
+          onChange={handleChange}
+        />
         <input
           type='password'
           name='confirmPassword'
           placeholder='Confirm password'
           className='form__input'
+          name='confirmPassword'
+          value={formData.confirmPassword}
+          onChange={handleChange}
         />
 
         <div className='form__marketing'>
-          <input type='checkbox' name='newsLetter' id='okayToEmail' />
+          <input
+            type='checkbox'
+            name='newsLetter'
+            id='okayToEmail'
+            name='newsLetter'
+            checked={formData.newsLetter}
+            onChange={handleChange}
+          />
           <label htmlFor='okayToEmail'>I want to join the newsletter</label>
         </div>
         <button className='form__submit'>Sign Up</button>
